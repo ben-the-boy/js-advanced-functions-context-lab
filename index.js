fa44c5f437be8e586186dd5c8634d4ed66bb815a
Ben Hayes <b.t.hayes@comcast.net>
2021-07-22 14:46:35 -0500

    commit tree bb45c008693a6977debeb5cfed7995ca380b4d8c

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeData) {
+  let employee = {
+    firstName: employeeData[0],
+    familyName: employeeData[1],
+    title: employeeData[2],
+    payPerHour: employeeData[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+
+  return employee;
+}
+
+function createEmployeeRecords(data) {
+  return data.map(function(employeeData) {
+    return createEmployeeRecord(employeeData);
+  })
+}
+
+function createTimeInEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(' ');
+
+  this.timeInEvents.push({
+    type: 'TimeIn',
+    hour: parseInt(hour),
+    date: date
+  })
+
+  return this
+}
+
+function createTimeOutEvent(dateStamp) {
+  let [date, hour] = dateStamp.split(' ');
+
+  this.timeOutEvents.push({
+    type: 'TimeOut',
+    hour: parseInt(hour),
+    date: date
+  })
+
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(e => e.date === date);
+
+  let timeOut = this.timeOutEvents.find(e => e.date === date);
+
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  let hours = hoursWorkedOnDate.call(this, date);
+
+  return this.payPerHour * hours;
+}
+
+function findEmployeeByFirstName(records, name) {
+  return records.find(employee => employee.firstName === name);
+}
+
+function calculatePayroll(records) {
+  return records.reduce(function(memo, record) {
+    return memo + allWagesFor.call(record);
+  }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
